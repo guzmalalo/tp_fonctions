c8b87c3024b429a31e1a8ee02ec5dd187d676aca
Eduardo Guzman <eduardo.guzman@innovamics.com>
2025-10-24 15:01:18 +0200

    feat: add triangle library

---
+++ triangle.h
@@ -0,0 +1,10 @@
+
+#ifndef TP_FONCTIONS_TRIANGLE_H
+#define TP_FONCTIONS_TRIANGLE_H
+#define TRIANGLE 2
+int initTriangle();
+float aireTriangle(int cote);
+void afficherTriangle(int cote);
+void Triangle(void);
+
+#endif //TP_FONCTIONS_TRIANGLE_H
